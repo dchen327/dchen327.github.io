62978de6634cda981e27372340693e02e941d2fe
David Chen <davidc3287@gmail.com>
2021-03-29 12:03:39 -0400

    add profile pic

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,17 +1,21 @@
-import Image from "next/image";
-import { Container, Divider, Grid, Header } from "semantic-ui-react";
+import { Container, Divider, Grid, Header, Image } from "semantic-ui-react";
 
 export default function About() {
   return (
-    <Container style={{ padding: "5%" }}>
+    <Container style={{ padding: "2%" }}>
       <Header size="large" style={{ textAlign: "center" }}>
         About Me
       </Header>
       <Divider />
-      <Grid>
-        <Grid.Row centered verticalAlign="middle">
+      <Grid
+        centered
+        columns="equal"
+        verticalAlign="middle"
+        style={{ padding: "0% 10% 0% 10%" }}
+      >
+        <Grid.Row>
           <Grid.Column width={4}>
-            <Image src="/images/david.jpg" layout="fill" />
+            <Image src="/images/david.jpg" verticalAlign="middle" />
           </Grid.Column>
           <Grid.Column width={12}>
             <p>Hi!</p>
