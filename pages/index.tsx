04f131e89555f33d73cb2b7fb5146206dc76c15f
David Chen <davidc3287@gmail.com>
2021-04-08 21:02:37 -0400

    clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,14 +25,3 @@ function Home() {
 }
 
 export default Home;
-
-// export default function Home() {
-//   return (
-//     <div>
-//       <Head>
-//         <title>David Chen</title>
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-//     </div>
-//   );
-// }
